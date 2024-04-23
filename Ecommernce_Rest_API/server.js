e9d3eb13d1c06ffc34dc9ca26c0afdf7f5e6f615
arshali2774 <arshali.763z@gmail.com>
2024-04-23 11:28:51 +0530

    sswagger 2.0

--- Ecommernce_Rest_API/server.js
+++ Ecommernce_Rest_API/server.js
@@ -5,13 +5,15 @@ import UserRouter from './src/features/user/routes/user.routes.js';
 import basicAuth from './src/middlewares/basicAuth.middleware.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import CartRouter from './src/features/cart/routes/cart.routes.js';
-
+import swagger from 'swagger-ui-express';
+import apiDocs from './swagger.json' assert { type: 'json' };
 const app = express();
 
 //middlewares
 app.use(bodyParser.json());
 
 // handle product routes
+app.use('/api-docs', swagger.serve, swagger.setup(apiDocs));
 app.use('/api/products', jwtAuth, ProductRouter);
 app.use('/api/users', UserRouter);
 app.use('/api/carts', jwtAuth, CartRouter);
